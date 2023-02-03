f46e48a96eb2d1989c49ca603fa29a136789f9fe
thakurshubhangi999 <101271228+thakurshubhangi999@users.noreply.github.com>
2023-02-03 16:05:35 +0530

    git commit -m"code changes"

--- js/script.js
+++ js/script.js
@@ -30,15 +30,5 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 
-  // const links = document.querySelectorAll('a[href*="#"]');
-  // for (const link of links) {
-  //   link.addEventListener("click", function(event) {
-  //     event.preventDefault();
-  //     const target = document.querySelector(this.getAttribute("href"));
-  //     window.scrollTo({
-  //       top: target.offsetTop,
-  //       behavior: "smooth"
-  //     });
-  //   });
-  // }
+
 });
